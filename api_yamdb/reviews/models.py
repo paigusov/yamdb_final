9abb9ed841818a0a9e5efd8327dc31436ba51898
paigusov <paigusov@gmail.com>
2022-12-04 19:53:02 +0300

    workflow

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-from reviews.validators import notlaterthisyearvalidatetor
+from .validators import notlaterthisyearvalidatetor
 
 User = get_user_model()
 
