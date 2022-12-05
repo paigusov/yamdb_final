4f652f37f9b0c80c214fb752caaa20f5b3ed7024
paigusov <paigusov@gmail.com>
2022-12-05 21:10:30 +0300

    1

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -12,7 +12,13 @@ SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 # DEBUG = os.getenv('DEBUG')
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '[::1]',
+    'testserver',
+    '51.250.96.207',
+    '*']
 
 
 # Application definition
